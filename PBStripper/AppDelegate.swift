11bbc69e862464f37d255157ba601ac32548ef9e
Igor Ławicki <igor.lawicki@gmail.com>
2018-10-01 16:53:56 +0200

    Updated project to Swift 4.2

--- PBStripper/AppDelegate.swift
+++ PBStripper/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         menubarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        let img = NSImage(named: NSImage.Name(rawValue: "stripper"))
+        let img = NSImage(named: "stripper")
         menubarItem.image = img
         menubarItem.action = #selector(menuIconClicked)
         pbManager.start()
